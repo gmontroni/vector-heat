ea0359f599cc083dc961dcc97e3cb3890d8b41db
gmontroni <montronig@gmail.com>
2025-03-10 15:55:52 -0300

    everything works

--- src/main.cpp
+++ src/main.cpp
@@ -95,9 +95,9 @@ int main(int argc, char **argv) {
   }
 
   // Create the solver (Points Cloud)
-  // double tCoef = 1.0;
-  // PointCloudHeatSolver PCHsolver(*cloud, *point_geometry, tCoef);
-  PointCloudHeatSolver pchSolver(*cloud, *point_geometry); // default tCoef = 1.0
+  double tCoef = 1.0;
+  PointCloudHeatSolver pchSolver(*cloud, *point_geometry, tCoef);
+  // PointCloudHeatSolver pchSolver(*cloud, *point_geometry); // default tCoef = 1.0
 
   // construct a solver (Mesh)
   VectorHeatMethodSolver vhmSolver(*geometry);
@@ -119,17 +119,78 @@ int main(int argc, char **argv) {
   // Set operator
   // Eigen::SparseMatrix<double> vectorOp = massMatcomplex + shortTime * cL; // vectorOp = operador M - tL
   // vectorHeatSolver.reset(new PositiveDefiniteSolver<double>(vectorOp));
+  
+  // Compute parallel transport (Points Cloud) 
+  Point pSource = cloud->point(587);
+  Vector2 sourcePoint{0.580121756, 0.814529777};
+  PointData<Vector2> pchTransport = pchSolver.transportTangentVector(pSource, sourcePoint);  // Only one source point
 
-  // Pick a source point
-  Point pSource = cloud->point(0);
+  PointData<Vector2> initialVec(*cloud);
+  initialVec[pSource] = sourcePoint;
 
-  // Compute parallel transport
-  Vector2 sourcePoint{1, 2};
-  PointData<Vector2> pchTransport = pchSolver.transportTangentVector(pSource, sourcePoint);
-
+  // Compute parallel transport (Mesh)
   Vertex vSource = mesh->vertex(0);
   Vector2 sourceVecMesh{1, 2};
-  VertexData<Vector2> vhmTransport = vhmSolver.transportTangentVector(vSource, sourceVecMesh);
+  VertexData<Vector2> vhmTransport = vhmSolver.transportTangentVector(vSource, sourceVecMesh);  // Only one source vertex
+
+  // Pick some source points
+  std::vector<std::vector<Point>> idxs;
+  idxs.push_back({cloud->point(600), cloud->point(1560), cloud->point(2520), cloud->point(3528), 
+                  cloud->point(3000), cloud->point(2039), cloud->point(72), cloud->point(1032)});
+  std::vector<std::vector<Vector2>> vecs2;
+  vecs2.push_back({ Vector2{-0.0683853626, 0.997658968}, Vector2{-0.0989825577, 0.995089114}, Vector2{-0.0809355006, -0.996719301}, Vector2{-0.0456265137, -0.998958528},
+                    Vector2{-0.0295587238, -0.999562979}, Vector2{-0.999998152, -0.0018937682}, Vector2{-0.999999583, -0.000849745411}, Vector2{-0.108760782, 0.994067907}});
+
+  // create a vector of tuples with points and vectors
+  std::vector<std::tuple<Point, Vector2>> sources;
+  // create a for acessing for each element of idxs and vecs2
+  for (size_t i = 0; i < idxs.size(); i++) {
+      for (size_t j = 0; j < idxs[i].size(); j++) {
+          sources.push_back(std::make_tuple(idxs[i][j], vecs2[i][j]));
+          // std::cout << "idxs elements: " << idxs[i][j] << std::endl;
+          // std::cout << "vecs2 elements: " << vecs2[i][j] << std::endl;
+      }
+  }
+
+  // Compute parallel transport with sources points
+  PointData<Vector2> pchTransportSourcesPoints = pchSolver.transportTangentVectors(sources);
+
+  // Create a null PointData<Vector2> object
+  PointData<Vector2> field(*cloud);
+  for (size_t i = 0; i < idxs.size(); i++) {
+    for (size_t j = 0; j < idxs[i].size(); j++) {
+      field[idxs[i][j]] = vecs2[i][j];
+    }
+  }
+
+  // Pick random source points and vectors
+  std::vector<std::vector<Point>> idxsrand;
+  idxsrand.push_back({cloud->point(0), cloud->point(1392), cloud->point(2881)});
+
+  std::vector<std::vector<Vector2>> vecs2rand;
+  vecs2rand.push_back({ Vector2{1, 2}, Vector2{3, 4}, Vector2{5, 6}});
+
+  // create a vector of tuples with points and vectors
+  std::vector<std::tuple<Point, Vector2>> sourcesrand;
+  for (size_t i = 0; i < idxsrand.size(); i++) {
+      for (size_t j = 0; j < idxsrand[i].size(); j++) {
+          sourcesrand.push_back(std::make_tuple(idxsrand[i][j], vecs2rand[i][j]));
+      }
+  }
+
+  // Compute parallel transport with Random Vectors
+  PointData<Vector2> pchTransportRandSources = pchSolver.transportTangentVectors(sourcesrand);
+
+  // Create a null PointData<Vector2> object
+  PointData<Vector2> fieldrand(*cloud);
+  for (size_t i = 0; i < idxsrand.size(); i++) {
+    for (size_t j = 0; j < idxsrand[i].size(); j++) {
+      fieldrand[idxsrand[i][j]] = vecs2rand[i][j];
+    }
+  }
+
+  // Compute Direction Field (Mesh)
+  VertexData<Vector2> directions = computeSmoothestVertexDirectionField(*geometry);
 
   // get home directory
   std::string homeDir = getenv("HOME");
@@ -138,33 +199,28 @@ int main(int argc, char **argv) {
   std::string lPath = filePath + "/laplacian_matrix.txt";
   std::string cLPath = filePath + "/connection_laplacian_matrix.txt";
 
-  // read an initial field (X0) from file
-  std::string line;
-  std::ifstream
-  file(filePath + "/X0.txt");
-  std::vector<Vector2> X0;
-  if (file.is_open()) {
-      while (std::getline(file, line)) {
-          std::istringstream iss(line);
-          double x, y;
-          iss >> x >> y;
-          X0.push_back(Vector2{x, y});
-      }
-      file.close();
-  } else {
-      std::cerr << "Unable to open file for reading" << std::endl;
-  }
+  // // read an initial field (X0) from file
+  // std::string line;
+  // std::ifstream
+  // file(filePath + "/X0.txt");
+  // std::vector<Vector2> X0;
+  // if (file.is_open()) {
+  //     while (std::getline(file, line)) {
+  //         std::istringstream iss(line);
+  //         double x, y;
+  //         iss >> x >> y;
+  //         X0.push_back(Vector2{x, y});
+  //     }
+  //     file.close();
+  // } else {
+  //     std::cerr << "Unable to open file for reading" << std::endl;
+  // }
 
-  // create a vector of tuples with points and vectors
+  // // create a vector of tuples with points and vectors
   // std::vector<std::tuple<Point, Vector2>> sources;
   // for (size_t i = 0; i < cloud->nPoints(); i++) {
   //     sources.push_back(std::make_tuple(cloud->point(i), X0[i]));
   // }
-
-  std::vector<std::tuple<Point, Vector2>> sources;
-  for (size_t i = 0; i < 47; i++) {
-      sources.push_back(std::make_tuple(cloud->point(i), X0[i]));
-  }
 
   // VertexData<Vector2> dField = computeSmoothestVertexDirectionField(*geometry);
   // std::vector<std::tuple<Point, Vector2>> sources;
@@ -172,7 +228,7 @@ int main(int argc, char **argv) {
   //     sources.push_back(std::make_tuple(cloud->point(i), dField[i]));
   // }
 
-  PointData<Vector2> X = pchSolver.transportTangentVectors(sources);
+  // PointData<Vector2> X = pchSolver.transportTangentVectors(sources);
 
   // save basis X to file
   std::ofstream basisXFile(filePath + "/basisX.txt");
@@ -291,11 +347,18 @@ int main(int argc, char **argv) {
 
   // Tufted triangulation plot
   polyscope::registerSurfaceMesh("Tufted Triangulation", vertexPositions, faceVertexList);
-
+  
+  // Add fields to the surface
+  psMesh->addVertexTangentVectorQuantity("Direction Field", directions, vBasisX, vBasisY);
   psMesh->addVertexTangentVectorQuantity("Parallel Transport with points cloud", pchTransport, pBasisX, pBasisY);
+  psMesh->addVertexTangentVectorQuantity("Parallel Transport with sources points", pchTransportSourcesPoints, pBasisX, pBasisY);
   psMesh->addVertexTangentVectorQuantity("Parallel Transport with mesh", vhmTransport, vBasisX, vBasisY);
-  psMesh->addVertexTangentVectorQuantity("X0", X0, pBasisX, pBasisY);
-  psMesh->addVertexTangentVectorQuantity("X", X, pBasisX, pBasisY);
+  psMesh->addVertexTangentVectorQuantity("Sources Points", field, pBasisX, pBasisY);
+  psMesh->addVertexTangentVectorQuantity("Parallel Transport with Random Vectors", pchTransportRandSources, pBasisX, pBasisY);
+  psMesh->addVertexTangentVectorQuantity("Único Vec", initialVec, pBasisX, pBasisY); 
+  psMesh->addVertexTangentVectorQuantity("Random Vectors", fieldrand, pBasisX, pBasisY);
+  // psMesh->addVertexTangentVectorQuantity("X0", X0, pBasisX, pBasisY);
+  // psMesh->addVertexTangentVectorQuantity("X", X, pBasisX, pBasisY);
   psMesh->addVertexVectorQuantity("Normal", pNormals);
   psMesh->addVertexVectorQuantity("Basis X", pBasisX);
   psMesh->addVertexVectorQuantity("Basis Y", pBasisY);
